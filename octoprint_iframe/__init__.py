2f7b05b71ef865abb34060c814ef164e00091b6d
spooknik <40317809+spooknik@users.noreply.github.com>
2024-12-17 13:46:50 +0100

    python3

--- octoprint_iframe/__init__.py
+++ octoprint_iframe/__init__.py
@@ -1,29 +1,28 @@
 # coding=utf-8
-from __future__ import absolute_import
-
 import octoprint.plugin
 
 class iFramePlugin(octoprint.plugin.StartupPlugin,
-                       octoprint.plugin.TemplatePlugin,
-                       octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin):
-	def on_after_startup(self):
-		self._logger.info("Set up and ready to go! (Default: %s)" % self._settings.get(["url"]))
+                    octoprint.plugin.TemplatePlugin,
+                    octoprint.plugin.SettingsPlugin,
+                    octoprint.plugin.AssetPlugin):
+    def on_after_startup(self):
+        self._logger.info(f"Set up and ready to go! (Default: {self._settings.get(['url'])})")
 
-	def get_settings_defaults(self):
-		return dict(url="https://en.m.wikipedia.org/wiki/Hello_world")
+    def get_settings_defaults(self):
+        return dict(url="https://en.m.wikipedia.org/wiki/Hello_world")
 
-	def get_template_configs(self):
-		return [
-			dict(type="settings", custom_bindings=False)
-		]
+    def get_template_configs(self):
+        return [
+            dict(type="settings", custom_bindings=False)
+        ]
 
-	def get_assets(self):
-		return dict(
-			js=["js/iframe.js"],
-			css=["css/iframe.css"],
-			less=["less/iframe.less"]
-		)
+    def get_assets(self):
+        return dict(
+            js=["js/iframe.js"],
+            css=["css/iframe.css"],
+            less=["less/iframe.less"]
+        )
 
 __plugin_name__ = "iFrame"
+__plugin_pythoncompat__ = ">=3.7,<4"
 __plugin_implementation__ = iFramePlugin()
